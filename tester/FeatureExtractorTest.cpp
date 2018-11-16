7906e2d63fe7ca896adbf2746a6fb20dbe9131a9
steve <551619855@qq.com>
2018-11-16 22:00:38 +0800

    try dense feature.

--- tester/FeatureExtractorTest.cpp
+++ tester/FeatureExtractorTest.cpp
@@ -35,6 +35,41 @@
 
 #include <util/GridFeatureExtractor.h>
 #include <opencv2/opencv.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+//模糊检测，如果原图像是模糊图像，返回0，否则返回1
+bool blurDetect(cv::Mat srcImage) {
+
+	cv::Mat gray1;
+	if (srcImage.channels() != 1) {
+		//进行灰度化
+		cv::cvtColor(srcImage, gray1, cv::COLOR_BGR2GRAY);
+	} else {
+		gray1 = srcImage.clone();
+	}
+	cv::Mat tmp_m1, tmp_sd1;    //用来存储均值和方差
+	double m1 = 0, sd1 = 0;
+	//使用3x3的Laplacian算子卷积滤波
+	cv::Laplacian(gray1, gray1, CV_16S, 3);
+	//归到0~255
+	cv::convertScaleAbs(gray1, gray1);
+	//计算均值和方差
+	cv::meanStdDev(gray1, tmp_m1, tmp_sd1);
+	m1 = tmp_m1.at<double>(0, 0);        //均值
+	sd1 = tmp_sd1.at<double>(0, 0);        //标准差
+	//cout << "原图像：" << endl;
+	std::cout << "均值: " << m1 << " , 方差: " << sd1 * sd1 << std::endl;
+	if (sd1 * sd1 < 100) {
+		std::cout << "原图像是模糊图像" << std::endl;
+		return true;
+	} else {
+		std::cout << "原图像是清晰图像" << std::endl;
+		return false;
+	}
+}
+
 
 int main() {
 
@@ -42,12 +77,6 @@ int main() {
 	stereo_camera_ptr->print("camera");
 
 //	BaseSLAM::VOSimple vo(stereo_camera_ptr);
-
-
-
-
-
-
 
 	BaseSLAM::MYNTVIDataReader data_reader("/home/steve/Data/MYNTVI/dataset-6f-simple");
 //	BaseSLAM::MYNTVIDataReader data_reader("/home/steve/Data/MYNTVI/dataset-5f-6f-easy");
@@ -68,6 +97,7 @@ int main() {
 //	cv::Ptr<cv::xfeatures2d::SiftFeatureDetector> detector = cv::xfeatures2d::SiftFeatureDetector::create(10000);
 	cv::Ptr<BaseSLAM::GridFeatureExtractor> detector = BaseSLAM::GridFeatureExtractor::create();
 
+	int clear_counter(0),blur_counter(0);
 
 	int i(0);
 	while (true) {
@@ -84,22 +114,37 @@ int main() {
 //		detector->detect(*(data->left_img_), left_key_points);
 //		detector->detect(*(data->right_img_), right_key_points);
 
-		detector->detect(*(data->left_img_), left_key_points);
-		detector->detect(*(data->right_img_), right_key_points);
+//		double score = cv::Laplacian(*(data->left_img_),cv::Mat(),0);
+		if (false&&blurDetect(*(data->left_img_))) {
+			std::cout << "failed" << std::endl;
+			cv::imwrite("/home/steve/temp/"+std::to_string(i)+".png",*(data->left_img_));
+			blur_counter++;
+		} else {
 
-		cv::drawKeypoints(*(data->left_img_), left_key_points, left_key_img);
-		cv::drawKeypoints(*(data->right_img_), right_key_points, right_key_img);
+			detector->detect(*(data->left_img_), left_key_points);
+			detector->detect(*(data->right_img_), right_key_points);
+
+			cv::drawKeypoints(*(data->left_img_), left_key_points, left_key_img);
+			cv::drawKeypoints(*(data->right_img_), right_key_points, right_key_img);
 
 
-		cv::imshow("left_key", left_key_img);
-		cv::imshow("right_key", right_key_img);
+			cv::imshow("left_key", left_key_img);
+			cv::imshow("right_key", right_key_img);
 
 
-		cv::waitKey(110);
-		std::cout << "index :" << i << std::endl;
+			cv::waitKey(10);
+			std::cout << "index :" << i << std::endl;
+			clear_counter++;
+		}
+
+//		std::cout << "score :" << score << std::endl;
+
+
+
 
 		++i;
 	}
+	std::cout << "clear :" << clear_counter << " blur :" << blur_counter << std::endl;
 
 
 }
--- util/GridFeatureExtractor.h
+++ util/GridFeatureExtractor.h
@@ -32,14 +32,139 @@
 #include <opencv2/xfeatures2d.hpp>
 
 namespace BaseSLAM {
+#define CLIP_RANGE(value, min, max)  ( (value) > (max) ? (max) : (((value) < (min)) ? (min) : (value)) )
+#define COLOR_RANGE(value)  CLIP_RANGE(value, 0, 255)
+
+	/**
+ *  \brief Automatic brightness and contrast optimization with optional histogram clipping
+ *  \param [in]src Input image GRAY or BGR or BGRA
+ *  \param [out]dst Destination image
+ *  \param clipHistPercent cut wings of histogram at given percent tipical=>1, 0=>Disabled
+ *  \note In case of BGRA image, we won't touch the transparency
+*/
+	void BrightnessAndContrastAuto(const cv::Mat &src, cv::Mat &dst, float clipHistPercent = 0) {
+		CV_Assert(clipHistPercent >= 0);
+		CV_Assert((src.type() == CV_8UC1) || (src.type() == CV_8UC3) || (src.type() == CV_8UC4));
+
+		int histSize = 256;
+		float alpha, beta;
+		double minGray = 0, maxGray = 0;
+
+		//to calculate grayscale histogram
+		cv::Mat gray;
+		if (src.type() == CV_8UC1) gray = src;
+		else if (src.type() == CV_8UC3) cvtColor(src, gray, cv::COLOR_GRAY2BGR);
+		else if (src.type() == CV_8UC4) cvtColor(src, gray, cv::COLOR_BGR2GRAY);
+		if (clipHistPercent == 0) {
+			// keep full available range
+			minMaxLoc(gray, &minGray, &maxGray);
+		} else {
+			cv::Mat hist; //the grayscale histogram
+
+			float range[] = {0, 256};
+			const float *histRange = {range};
+			bool uniform = true;
+			bool accumulate = false;
+			calcHist(&gray, 1, 0, cv::Mat(), hist, 1, &histSize, &histRange, uniform, accumulate);
+
+			// calculate cumulative distribution from the histogram
+			std::vector<float> accumulator(histSize);
+			accumulator[0] = hist.at<float>(0);
+			for (int i = 1; i < histSize; i++) {
+				accumulator[i] = accumulator[i - 1] + hist.at<float>(i);
+			}
+
+			// locate points that cuts at required value
+			float max = accumulator.back();
+			clipHistPercent *= (max / 100.0); //make percent as absolute
+			clipHistPercent /= 2.0; // left and right wings
+			// locate left cut
+			minGray = 0;
+			while (accumulator[minGray] < clipHistPercent)
+				minGray++;
+
+			// locate right cut
+			maxGray = histSize - 1;
+			while (accumulator[maxGray] >= (max - clipHistPercent))
+				maxGray--;
+		}
+
+		// current range
+		float inputRange = maxGray - minGray;
+
+		alpha = (histSize - 1) / inputRange;   // alpha expands current range to histsize range
+		beta = -minGray * alpha;             // beta shifts current range so that minGray will go to 0
+
+		// Apply brightness and contrast normalization
+		// convertTo operates with saurate_cast
+		src.convertTo(dst, -1, alpha, beta);
+
+		// restore alpha channel from source
+		if (dst.type() == CV_8UC4) {
+			int from_to[] = {3, 3};
+			mixChannels(&src, 4, &dst, 1, from_to, 1);
+		}
+		return;
+	}
+
+/**
+ * Adjust Brightness and Contrast
+ *
+ * @param src [in] InputArray
+ * @param dst [out] OutputArray
+ * @param brightness [in] integer, value range [-255, 255]
+ * @param contrast [in] integer, value range [-255, 255]
+ *
+ * @return 0 if success, else return error code
+ */
+	int adjustBrightnessContrast(cv::InputArray src, cv::OutputArray dst, int brightness, int contrast) {
+		cv::Mat input = src.getMat();
+		if (input.empty()) {
+			return -1;
+		}
+
+		dst.create(src.size(), src.type());
+		cv::Mat output = dst.getMat();
+
+		brightness = CLIP_RANGE(brightness, -255, 255);
+		contrast = CLIP_RANGE(contrast, -255, 255);
+
+		/**
+		Algorithm of Brightness Contrast transformation
+		The formula is:
+			y = [x - 127.5 * (1 - B)] * k + 127.5 * (1 + B);
+
+			x is the input pixel value
+			y is the output pixel value
+			B is brightness, value range is [-1,1]
+			k is used to adjust contrast
+				k = tan( (45 + 44 * c) / 180 * PI );
+				c is contrast, value range is [-1,1]
+		*/
+
+		double B = brightness / 255.;
+		double c = contrast / 255.;
+		double k = tan((45 + 44 * c) / 180 * M_PI);
+
+		cv::Mat lookupTable(1, 256, CV_8U);
+		uchar *p = lookupTable.data;
+		for (int i = 0; i < 256; i++)
+			p[i] = COLOR_RANGE((i - 127.5 * (1 - B)) * k + 127.5 * (1 + B));
+
+		LUT(input, lookupTable, output);
+
+		return 0;
+	}
+
+
 	class GridFeatureExtractor {
 	public:
-		int row_size_ = 200;
-		int col_size_ = 200;
-		int over_row_ = 50;
-		int over_col = 50;
+		int row_size_ = 100;
+		int col_size_ = 100;
+		int over_row_ = 30;
+		int over_col = 30;
 
-		int min_batch_feature_num_ = 50;
+		int min_batch_feature_num_ = 20;
 
 
 		static cv::Ptr<GridFeatureExtractor> create() {
@@ -75,6 +200,7 @@ namespace BaseSLAM {
 //			int row_offset(0), col_offset(0);
 
 
+
 			for (int row_offset = 0; row_offset < image_rows; row_offset += (row_size_ - over_row_)) {
 				for (int col_offset = 0; col_offset < image_cols; col_offset = col_offset + col_size_ - over_col) {
 //					std::cout << "row offset :" << row_offset << " row_size:" << row_size_ << " over row:"
@@ -93,17 +219,25 @@ namespace BaseSLAM {
 					              cv::Range(col_offset, col_offset + col_size_ - cut_col)
 					).clone();
 
-					std::vector<cv::KeyPoint> tmp;
+//					cv::imshow("sub img be", sub_img);
+					BrightnessAndContrastAuto(sub_img, sub_img, 0.5);
+//					cv::imshow("sub img af",sub_img);
+//					cv::waitKey(0);
 
-					int threshold = 15;
-					while (tmp.size() < min_batch_feature_num_ && threshold > 5) {
+					std::vector<cv::KeyPoint> tmp;
+//
+//					int threshold = 15;
+//					while (tmp.size() < min_batch_feature_num_ && threshold > 20) {
 //						normal_detector->setThreshold(threshold);
-						normal_detector->setEdgeThreshold(threshold);
+//						normal_detector->setEdgeThreshold(threshold);
+//						normal_detector->setFastThreshold(threshold);
 //						normal_detector->setThreshold(double(threshold-4)/10);//AKAZE feature
 
-						normal_detector->detect(sub_img, tmp);
-						threshold = threshold -1;
-					}
+//						normal_detector->detect(sub_img, tmp);
+//						threshold = threshold - 1;
+//					}
+
+					normal_detector->detect(sub_img,tmp);
 //					std::cout << "tmp size:" << tmp.size()
 //					          << " threshold:" << threshold << std::endl;
 
@@ -120,8 +254,17 @@ namespace BaseSLAM {
 //					std::cout << "col offset:" << col_offset << "row offset :" << row_offset << std::endl;
 
 
+
+
 				}
 			}
+
+			cv::Mat tmp_res;
+
+//			adjustBrightnessContrast(img,tmp_res,)
+			BrightnessAndContrastAuto(img, tmp_res, 0.05);
+			cv::imshow("before auto ", img);
+			cv::imshow("auto", tmp_res);
 
 
 			return true;
