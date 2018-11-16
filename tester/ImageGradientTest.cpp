d94b8e7d8d8d21f64b01ddf85ee1fe139955ddc3
steve <551619855@qq.com>
2018-11-16 15:56:36 +0800

    try dense feature.

--- tester/ImageGradientTest.cpp
+++ tester/ImageGradientTest.cpp
@@ -49,8 +49,8 @@ int main() {
 
 
 
-//	BaseSLAM::MYNTVIDataReader data_reader("/home/steve/Data/MYNTVI/dataset-6f-simple");
-	BaseSLAM::MYNTVIDataReader data_reader("/home/steve/Data/MYNTVI/dataset-5f-6f-easy");
+	BaseSLAM::MYNTVIDataReader data_reader("/home/steve/Data/MYNTVI/dataset-6f-simple");
+//	BaseSLAM::MYNTVIDataReader data_reader("/home/steve/Data/MYNTVI/dataset-5f-6f-easy");
 
 	cv::namedWindow("show left");
 	cv::namedWindow("show right");
@@ -93,12 +93,13 @@ int main() {
 		cv::cvtColor(*(data->right_img_), right_key_img, cv::COLOR_GRAY2BGR);
 //		left_key_img=data->left_img_->clone();
 //		right_key_img = data->right_img_->clone();
-		double threshold = 15.0;
+		double threshold = 30.0;
 		left_key_points.clear();
 		right_key_points.clear();
-
-#pragma omp parallel for
-		for (int i=(1); i < data->left_img_->cols - 1; i++) {
+		std::vector<cv::Point2f> left_points, right_points;
+		std::vector<cv::Point2f> track_left_points, track_right_points;
+//#pragma omp parallel for
+		for (int i = (1); i < data->left_img_->cols - 1; i++) {
 			for (int j(1); j < data->left_img_->rows - 1; j++) {
 				Eigen::Vector2d left_grad(
 						data->left_img_->ptr<uchar>(j)[i + 1] - data->left_img_->ptr<uchar>(j)[i - 1],
@@ -109,44 +110,87 @@ int main() {
 						data->right_img_->ptr<uchar>(j + 1)[i] - data->right_img_->ptr<uchar>(j - 1)[i]
 				);
 				if (left_grad.norm() > threshold) {
-//					left_key_points.push_back(cv::KeyPoint(i, j, 1));
-					cv::ellipse(left_key_img, cv::Point2d(i,j), cv::Size(4, 4), 0.0, 0.0, 0.0,
+					left_key_points.push_back(cv::KeyPoint(i, j, 1));
+					left_points.push_back(cv::Point2f(i,j));
+					cv::ellipse(left_key_img, cv::Point2d(i, j), cv::Size(4, 4), 0.0, 0.0, 0.0,
 					            cv::Scalar(200, 200, 0));
 
 				}
 
 
 				if (right_grad.norm() > threshold) {
-//					right_key_points.push_back(cv::KeyPoint(i, j, 1));
+					right_key_points.push_back(cv::KeyPoint(i, j, 1));
+					left_points.push_back(cv::Point2d(i,j));
 					cv::ellipse(right_key_img, cv::Point2d(i, j), cv::Size(4, 4), 0.0, 0.0, 0.0,
 					            cv::Scalar(200, 200, 0));
 				}
 
 
-
 			}
 		}
-//		cv::Mat prev_pts(left_key_points.size(),2);
-//		for(int i(0);i<left_key_points.size();++i){
-//			prev_pts(i,0) =
-//		}
-
-//		cv::calcOpticalFlowPyrLK(*(data->left_img_),*(data->right_img_),)
 
 
-		cv::Mat two_mat(data->left_img_->rows,data->right_img_->cols*2,0,cv::Scalar(0,0,0));
+
+
+		std::vector<uchar> status;
+		std::vector<float> err;
+
+//		for(int i(0);i<left_key_points.size())
+
+
+		cv::calcOpticalFlowPyrLK(*(data->left_img_), *(data->right_img_),
+				left_points,track_left_points,status,err);
+
+
+
+
+		cv::Mat two_mat(data->left_img_->rows, data->right_img_->cols * 2, CV_8UC3, cv::Scalar(0, 0, 0));
+
 
 //		for(int i(0);i<data->left_img_->rows;++i){
 //			two_mat.row(i) = data->left_img_->row(i).clone();
 //			two_mat.row(i+data->left_img_->rows) = data->right_img_->row(i).clone();
 //		}
-//#pragma omp parallel for
-		for(int i=0;i<data->left_img_->rows;++i){
-			for(int j=0;j<data->left_img_->cols;++j){
-				two_mat.at<cv::Vec3b>(i,j) = left_key_img.at<cv::Vec3b>(i,j);
-				two_mat.at<cv::Vec3b>(i,j+left_key_img.cols) = right_key_img.at<cv::Vec3b>(i,j);
+#pragma omp parallel for
+		for (int i = 0; i < data->left_img_->rows; ++i) {
+			for (int j = 0; j < data->left_img_->cols; ++j) {
+				two_mat.at<cv::Vec3b>(i, j) = left_key_img.at<cv::Vec3b>(i, j);
+				two_mat.at<cv::Vec3b>(i, j + left_key_img.cols) = right_key_img.at<cv::Vec3b>(i, j);
 			}
 		}
+
+		//getting rid of points for which the KLT tracking failed or those who have gone outside the frame
+		int indexCorrection = 0;
+		for( int i=0; i<status.size(); i++)
+		{  cv::Point2f pt = left_points.at(i- indexCorrection);
+			if ((status.at(i) == 0)||(pt.x<0)||(pt.y<0))	{
+				if((pt.x<0)||(pt.y<0))	{
+					status.at(i) = 0;
+				}
+				left_points.erase (left_points.begin() + (i - indexCorrection));
+				track_left_points.erase (track_left_points.begin() + (i - indexCorrection));
+				err.erase(err.begin()+(i-indexCorrection));
+				indexCorrection++;
+			}
+
+		}
+
+		int error_counter = 0;
+		for(int i=0;i<left_points.size();++i){
+			cv::line(two_mat,
+					left_points[i],
+					cv::Point2f(track_left_points[i].x+left_key_img.cols,track_left_points[i].y),
+					cv::Scalar(0,200,20));
+			if(left_points[i].y-track_left_points[i].y>5){
+				error_counter++;
+							cv::line(two_mat,
+					left_points[i],
+					cv::Point2f(track_left_points[i].x+left_key_img.cols,track_left_points[i].y),
+					cv::Scalar(0,0,200));
+			}
+		}
+
+
 
 		cv::imshow("tw img", two_mat);
 
@@ -155,7 +199,7 @@ int main() {
 //		std::cout << "left size :" << data->left_img_->size << std::endl;
 //		std::cout << "left key img size:" << left_key_img.size << std::endl;
 
-		cv::imshow("left show",*(data->left_img_));
+		cv::imshow("left show", *(data->left_img_));
 
 		cv::imshow("left_key", left_key_img);
 		cv::imshow("right_key", right_key_img);
