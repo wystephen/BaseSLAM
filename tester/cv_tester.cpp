a72ccbbf8898e6c100ab8048da36170a43c810dd
wang stephen <551619855@qq.com>
2018-11-18 17:45:21 +0800

    test on WSL...run well.

--- tester/cv_tester.cpp
+++ tester/cv_tester.cpp
@@ -30,6 +30,23 @@
 using namespace cv;
 int main(int argc, char** argv )
 {
+
+	try{
+
+		namedWindow("Display Image", WINDOW_AUTOSIZE );
+		cv::Mat tmp_mat(1000,1000,0,cv::Scalar(200));
+
+		for(int i(100);i<900;++i){
+			tmp_mat(cv::Range(i-55,i+55),cv::Range(i-55,i+55))= 0;
+			std::cout << "tmp mat:" << i<< std::endl;
+		}
+		imshow("Display Image", tmp_mat);
+		cv::waitKey(0);
+
+	}catch (std::exception &e){
+		std::cout << e.what() <<std::endl;
+	}
+	std::cout << "after create image windows" << std::endl;
 	if ( argc != 2 )
 	{
 		printf("usage: DisplayImage.out <Image_Path>\n");
