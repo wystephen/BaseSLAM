365c571078ed61ebcc1e591d1a8390e129b619f4
steve <551619855@qq.com>
2019-01-28 17:03:07 +0800

    test

--- tester/KeypointsTest.cpp
+++ tester/KeypointsTest.cpp
@@ -34,11 +34,10 @@
 
 #include <opencv2/opencv.hpp>
 
-int main()
-{
+int main() {
 
-    auto *stereo_camera_ptr = new BaseSLAM::StereoCamera ( "/home/steve/Data/MYNTVI/camera_parameter1.yaml" );
-    stereo_camera_ptr->print ( "camera" );
+	auto *stereo_camera_ptr = new BaseSLAM::StereoCamera("/home/steve/Data/MYNTVI/camera_parameter1.yaml");
+	stereo_camera_ptr->print("camera");
 
 //	BaseSLAM::VOSimple vo(stereo_camera_ptr);
 
@@ -46,54 +45,54 @@ int main()
 
 
 //	BaseSLAM::MYNTVIDataReader data_reader("/home/steve/Data/MYNTVI/dataset-6f-simple");
-    BaseSLAM::MYNTVIDataReader data_reader ( "/home/steve/Data/MYNTVI/dataset-5f-6f-easy" );
+	BaseSLAM::MYNTVIDataReader data_reader("/home/steve/Data/MYNTVI/dataset-5f-6f-easy");
 
-    cv::namedWindow ( "show left" );
-    cv::namedWindow ( "show right" );
+	cv::namedWindow("show left");
+	cv::namedWindow("show right");
 
-    // Create carema model and load parameters.
-
-
-    std::vector<cv::KeyPoint> left_key_points, right_key_points;
-    cv::Mat left_key_img,right_key_img;
-
-    cv::Ptr<cv::xfeatures2d::SiftFeatureDetector> detector = cv::xfeatures2d::SiftFeatureDetector::create ( 1000 );
-
-    // cv::Ptr<cv::AgastFeatureDetector> detector = cv::AgastFeatureDetector::create(10);
+	// Create carema model and load parameters.
 
 
+	std::vector<cv::KeyPoint> left_key_points, right_key_points;
+	cv::Mat left_key_img, right_key_img;
+
+	cv::Ptr<cv::xfeatures2d::SiftFeatureDetector> detector = cv::xfeatures2d::SiftFeatureDetector::create(1000);
+
+	// cv::Ptr<cv::AgastFeatureDetector> detector = cv::AgastFeatureDetector::create(10);
 
 
 
-    int i ( 0 );
-    while ( true ) {
-        auto *data = data_reader.get_data ( i );
-        if ( data == nullptr ) {
-            std::cout << "finished" << std::endl;
-            break;
-        }
+
+
+	int i(0);
+	while (true) {
+		auto *data = data_reader.get_data(i);
+		if (data == nullptr) {
+			std::cout << "finished" << std::endl;
+			break;
+		}
 //		std::cout << "readed image" << std::endl;
 //		cv::imshow("show left",*(data->left_img_ptr_));
 //		cv::imshow("show right", *(data->right_img_ptr_));
 //		std::cout << " after imshow" << std::endl;
 //		vo.addNewFrame(data);
-        detector->detect ( * ( data->left_img_ptr_ ),left_key_points );
-        detector->detect ( * ( data->right_img_ptr_ ),right_key_points );
+		detector->detect(*(data->left_img_ptr_), left_key_points);
+		detector->detect(*(data->right_img_ptr_), right_key_points);
 
 //		agast_detector->detect(*(data->left_img_ptr_),left_key_points);
 //		agast_detector->detect(*(data->right_img_ptr_),right_key_points);
 
-        cv::drawKeypoints ( * ( data->left_img_ptr_ ),left_key_points,left_key_img );
-        cv::drawKeypoints ( * ( data->right_img_ptr_ ),right_key_points,right_key_img );
+		cv::drawKeypoints(*(data->left_img_ptr_), left_key_points, left_key_img);
+		cv::drawKeypoints(*(data->right_img_ptr_), right_key_points, right_key_img);
 
-        cv::imshow ( "left_key",left_key_img );
-        cv::imshow ( "right_key",right_key_img );
+		cv::imshow("left_key", left_key_img);
+		cv::imshow("right_key", right_key_img);
 
-        cv::waitKey ( 110 );
-        std::cout << "index :" << i << std::endl;
+		cv::waitKey(110);
+		std::cout << "index :" << i << std::endl;
 
-        ++i;
-    }
+		++i;
+	}
 
 
 }
