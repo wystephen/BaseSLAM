7a13e8daa1497f05d34440349f99ea86fbde0b08
steve <551619855@qq.com>
2019-02-04 17:55:20 +0800

    changed data loader model.

--- Unit_Test/TEST_FeatureTrackServer.cpp
+++ Unit_Test/TEST_FeatureTrackServer.cpp
@@ -7,30 +7,33 @@
 #include "../../googletest/googletest/include/gtest/gtest.h"
 
 
-class FeatureTrackServertest: public ::testing::Test{
+class FeatureTrackServertest : public ::testing::Test {
 
 public:
+
+};
+
+TEST_F(FeatureTrackServertest, test_reduce_vec) {
 	std::vector<int> in_vec;
 	std::vector<uchar> mask_vec;
 	std::vector<int> ref_out_vec;
-};
-
-TEST_F(FeatureTrackServertest, test_reduce_vec){
-		for(int i=0;i<100;++i){
+	for (int i = 0; i < 100; ++i) {
 		in_vec.push_back(i);
-		if(i%2==0){
+		if (i % 2 == 0) {
 			mask_vec.push_back(0);
 			ref_out_vec.push_back(i);
 
-		}else{
+		} else {
 			mask_vec.push_back(1);
 		}
 
 	}
 
-	reduceVector<int>(in_vec,mask_vec);
+	reduceVector<int>(in_vec, mask_vec);
 	bool flag = true;
-	ASSERT_TRUE(in_vec.size()==ref_out_vec.size());
+	std::cout << "in_vec size:" << in_vec.size()
+	          << "/n ref out vec size:" << ref_out_vec.size() << std::endl;
+	ASSERT_TRUE(in_vec.size() == ref_out_vec.size());
 
 }
 
