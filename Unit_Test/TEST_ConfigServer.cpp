1745286510528fac660f30e58d9941f58938c1bf
steve <551619855@qq.com>
2019-02-05 16:30:57 +0800

    changed data loader model.

---
+++ Unit_Test/TEST_ConfigServer.cpp
@@ -0,0 +1,21 @@
+//
+// Created by steve on 19-2-5.
+//
+
+#include <gtest/gtest.h>
+
+#include <util/ConfigServer.h>
+
+#include "../../googletest/googletest/include/gtest/gtest.h"
+
+
+class ConfigServertest : public ::testing::Test {
+
+public:
+
+};
+
+
+TEST_F(ConfigServertest,LoadTest){
+
+}
